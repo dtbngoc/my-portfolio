afd1af73764e16fd00db5f3ea986aade01138ec8
Ngoc Dang <dangtb.ngoc@gmail.com>
2019-01-23 16:47:20 +0700

    change something

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,14 +6,10 @@ import Image from '../components/image'
 
 const IndexPage = () => (
   <Layout>
-    <div className="Hero">
-      <div className="HeroGroup">
-        <h1>I am a multidisciplinary designer based in Vietnam, focused on UI and UX Design, Web Design and Development.</h1>
-        <p>Now go build something great.</p>
-        {/* <div style={{ maxWidth: '300px', marginBottom: '1.45rem' }}>
-          <Image />
-        </div> */}
-        <Link to="/page-2/">Go to page 2</Link>
+    <div className="Intro">
+      <div className="IntroGroup">
+        <h1>I create products not just art</h1>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut.</p>
       </div>
     </div>
 
