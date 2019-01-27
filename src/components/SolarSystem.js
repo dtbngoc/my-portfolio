9bbd585bd2f4bed1cbffcf8ea886a7f8b69f4de9
Ngoc Dang <dangtb.ngoc@gmail.com>
2019-01-27 19:41:26 +0700

    landing

--- src/components/SolarSystem.js
+++ src/components/SolarSystem.js
@@ -2,110 +2,75 @@ import React from 'react'
 
 const SolarSystem = props => (
     <div className='SolarSystem'>
-         <div class="view-3D">
-            <div id="universe" class="scale-stretched">
-                <div id="galaxy">
-                    <div id="solar-system" class="earth">
-                    <div id="mercury" class="orbit">
-                        <div class="pos">
-                        <div class="planet">
-                            <dl class="infos">
-                            <dt>Mercury</dt>
-                            <dd><span></span></dd>
-                            </dl>
-                        </div>
-                        </div>
-                    </div>
-                    <div id="venus" class="orbit">
-                        <div class="pos">
-                        <div class="planet">
-                            <dl class="infos">
-                            <dt>Venus</dt>
-                            <dd><span></span></dd>
-                            </dl>
-                        </div>
-                        </div>
-                    </div>
-                    
+        <div className='OrbitRingGroup'>
+          <div id='ring-1' className='OrbitRing' />
+          <div id='ring-2' className='OrbitRing' />
+          <div id='ring-3' className='OrbitRing' />
+          <div id='ring-4' className='OrbitRing' />
+          <div id='ring-5' className='OrbitRing' />
+          <div id='ring-6' className='OrbitRing' />
+          <div id='ring-7' className='OrbitRing' />
+          
+        </div>
+        <div class='view-3D'>
+            <div id='solar-system'>
+                <div id='sun' class='no-clipping' />
+                <div id='sun' class='no-clipping' />
+            </div>
+        </div>
+        <div className='OrbitRingGroup'>
+            <div id='ring-1' className='OrbitRingFront' />
+            <div id='ring-2' className='OrbitRingFront' />
+            <div id='ring-3' className='OrbitRingFront' />
+            <div id='ring-4' className='OrbitRingFront' />
+            <div id='ring-5' className='OrbitRingFront' />
+        </div>
 
 
-                        <div id="earth" class="orbit">
-                            <div class="pos">
-                                <div class="orbit">
-                                    <div class="pos">
-                                        <div class="moon" />
-                                    </div>
-                                </div>
-                            <div class="planet">
-                                <dl class="infos">
-                                    <dt>Earth</dt>
-                                </dl>
-                            </div>
-                            </div>
-                        </div>
-
-
-                        <div id="mars" class="orbit">
-                            <div class="pos">
-                            <div class="planet">
-                                <dl class="infos">
-                                <dt>Mars</dt>
-                                <dd><span></span></dd>
-                                </dl>
-                            </div>
-                            </div>
-                        </div>
-                        <div id="jupiter" class="orbit">
-                            <div class="pos">
-                            <div class="planet">
-                                <dl class="infos">
-                                <dt>Jupiter</dt>
-                                <dd><span></span></dd>
-                                </dl>
-                            </div>
-                            </div>
-                        </div>
-                        <div id="saturn" class="orbit">
-                            <div class="pos">
-                            <div class="planet">
-                               
-                                <dl class="infos">
-                                <dt>Saturn</dt>
-                                <dd><span></span></dd>
-                                </dl>
-                            </div>
-                            </div>
-                        </div>
-                        <div id="uranus" class="orbit">
-                            <div class="pos">
-                            <div class="planet">
-                                <dl class="infos">
-                                <dt>Uranus</dt>
-                                <dd><span></span></dd>
-                                </dl>
-                            </div>
-                            </div>
-                        </div>
-
-
-                        <div id="neptune" class="orbit">
-                            <div class="pos">
-                            <div class="planet">
-                                <dl class="infos">
-                                <dt>Neptune</dt>
-                                <dd><span></span></dd>
-                                </dl>
-                            </div>
-                            </div>
-                        </div>
-
-                        
-                  
-                   
-                        <div id="sun" />
+        <div class='view-3D'>
+            <div id='solar-system'>
+                <div id='sun' class='sun-clipping'/>
+                <div id='mercury' class='orbit'>
+                    <div class='pos'>
+                        <div class='planet' />
                     </div>
                 </div>
+                <div id='venus' class='orbit'>
+                    <div class='pos'>
+                        <div class='planet' />
+                    </div>
                 </div>
+                <div id='earth' class='orbit'>
+                    <div class='pos'>
+                        <div class='orbit'>
+                            <div class='pos'>
+                                <div class='moon' />
+                            </div>
+                        </div>
+                        <div class='planet' />
+                    </div>
+                </div>
+                <div id='mars' class='orbit'>
+                    <div class='pos'>
+                        <div class='planet' />
+                    </div>
+                </div>
+                <div id='jupiter' class='orbit'>
+                    <div class='pos'>
+                        <div class='planet' />
+                    </div>
+                </div>
+                <div id='saturn' class='orbit'>
+                    <div class='pos'>
+                        <div class='planet' />
+                    </div>
+                </div>
+                <div id='uranus' class='orbit'>
+                    <div class='pos'>
+                        <div class='planet' />
+                    </div>
+                </div>
+            </div>
         </div>
     </div>
 )
