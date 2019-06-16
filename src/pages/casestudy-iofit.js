a3ab7c2a46c0f5fda16d602bf97b08762054e7d2
Ngoc Dang <dangtb.ngoc@gmail.com>
2019-06-16 18:58:08 +0700

    case01 and optimize

--- src/pages/casestudy-iofit.js
+++ src/pages/casestudy-iofit.js
@@ -1,25 +1,121 @@
-//import Layout from '../components/layout'
-//import "../components/layout.scss"
 import React from 'react'
+
+import Img from "gatsby-image"
+import { graphql } from "gatsby"
+
 import AniLink from 'gatsby-plugin-transition-link/AniLink'
+import Fade from 'react-reveal/Fade'
 
-import Button from '../components/Button'
 
-const IofitPage  = () => (
-   // <Layout>
-    <div className='Iofit'>
-        <AniLink cover to='/#case02' direction='right' bg='#ECEDFB'>
-            <div className='Header'>
-                <div className='link-container'>
-                    <Button text='Back' id='on-black'/>
-                    <Button text='Back' id='on-black'/>
+const IofitPage  = ({ data }) => (
+    <div className='IofitPage'>
+        <div id='noise-background' className='page-container'>
+            <AniLink cover to='/#case02' direction='right' bg='#111421'>
+                <div className='back-button'>
+                    <svg width="30" height="11" viewBox="0 0 30 11" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M29.1992 9.69629H1.64449L9.64423 1.69629" />
+                    </svg>
+                    <span>Back</span>
                 </div>
+            </AniLink>
+
+            <div className='content-container'>
+                <Fade bottom>
+                    <section>
+                        <Img fluid={data.img01.childImageSharp.fluid} />
+                    </section>
+                </Fade> 
+                <Fade bottom>
+                    <section>
+                        <Img fluid={data.img02.childImageSharp.fluid} />
+                    </section>
+                </Fade> 
+                <Fade bottom>
+                    <section>
+                        <Img fluid={data.img03.childImageSharp.fluid} />
+                    </section>
+                </Fade> 
+                <Fade bottom>
+                    <section>
+                        <Img fluid={data.img0401.childImageSharp.fluid} />
+                    </section>
+                </Fade> 
+                
+                <Fade bottom>
+                    <div className='vimeo-video'>
+                        <iframe src="https://player.vimeo.com/video/342544098?autoplay=1&loop=1&title=0&byline=0&portrait=0" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div> 
+                </Fade>
+               
+                <Fade bottom>
+                    <section>
+                        <img width='100%' src={require('../images/02_iofit/05_01_menubar.gif')} alt='gif'/>
+                    </section>
+                </Fade> 
+                <Fade bottom>
+                    <div className='vimeo-video'>
+                        <iframe src="https://player.vimeo.com/video/342547531?autoplay=1&loop=1&title=0&byline=0&portrait=0" frameborder="0" allow="autoplay; fullscreen" allowfullscreen></iframe>
+                    </div> 
+                </Fade>
             </div>
             
-        </AniLink>
-            
+        </div>
     </div>
-   // </Layout>
 )
 
 export default IofitPage
+
+
+export const query = graphql`
+    query {
+        img01: file(
+            relativePath: { regex: "/02_iofit/01_header/" }
+        ) {
+            childImageSharp {
+                fluid(
+                    maxWidth: 1920
+                ) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+
+        img02: file(
+            relativePath: { regex: "/02_iofit/02_sitemap/" }
+        ) {
+            childImageSharp {
+                fluid(
+                    maxWidth: 1920
+                ) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+
+        img03: file(
+            relativePath: { regex: "/02_iofit/03_color/" }
+        ) {
+            childImageSharp {
+                fluid(
+                    maxWidth: 1920
+                ) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+
+        img0401: file(
+            relativePath: { regex: "/02_iofit/04_01_onboarding/" }
+        ) {
+            childImageSharp {
+                fluid(
+                    maxWidth: 1920
+                ) {
+                    ...GatsbyImageSharpFluid
+                }
+            }
+        }
+
+       
+    }
+`
